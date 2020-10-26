a80309763ad4bfb7280f0c29062fdb113c264070
Kashiff Khan <kashiff.k@yahoo.com>
2020-10-26 19:32:39 -0400

    changed file name

--- client/src/screens/PostCreate/postcreate.jsx
+++ client/src/screens/PostCreate/postcreate.jsx
@@ -30,39 +30,37 @@ const PostCreate = (props) => {
     return <Redirect to={`/posts`} />;
   }
   return (
-    <div>
-      <form className="create-form" onSubmit={handleSubmit}>
-        <input
-          className="input-title"
-          placeholder="title"
-          value={post.title}
-          name="title"
-          required
-          autoFocus
-          onChange={handleChange}
-        />
-        <input
-          className="input-author"
-          placeholder="author"
-          value={post.author}
-          name="author"
-          required
-          onChange={handleChange}
-        />
-        <textarea
-          className="textarea-content"
-          rows={10}
-          placeholder="content"
-          value={post.content}
-          name="content"
-          required
-          onChange={handleChange}
-        />
-        <button type="submit" className="submit-button">
-          Submit
-        </button>
-      </form>
-    </div>
+    <form className="create-form" onSubmit={handleSubmit}>
+      <input
+        className="input-title"
+        placeholder="title"
+        value={post.title}
+        name="title"
+        required
+        autoFocus
+        onChange={handleChange}
+      />
+      <input
+        className="input-author"
+        placeholder="author"
+        value={post.author}
+        name="author"
+        required
+        onChange={handleChange}
+      />
+      <textarea
+        className="textarea-content"
+        rows={10}
+        placeholder="content"
+        value={post.content}
+        name="content"
+        required
+        onChange={handleChange}
+      />
+      <button type="submit" className="submit-button">
+        Submit
+      </button>
+    </form>
   );
 };
 
